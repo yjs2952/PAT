79b2b31806f74501cd04bd0251a56c4067805fc0
siyoon <jung_210@naver.com>
2018-12-14 17:30:58 +0900

    feat: 책 조회하기 (이름으로 검색, Id로 검색) 레퍼지토리 메소드 추가

--- injucksung/src/main/java/com/injucksung/injucksung/repository/BookRepository.java
+++ injucksung/src/main/java/com/injucksung/injucksung/repository/BookRepository.java
@@ -7,8 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-    //모든 책 목록 조회하기
-
     //책 이름으로 검색하여 책 목록 조회하기
     Page<Book> findBookByNameContaining(String name, Pageable pageable);
 
