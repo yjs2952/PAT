3060fa0d53052785cb26b7bf80130730e5dc4902
siyoon <jung_210@naver.com>
2018-12-17 14:22:15 +0900

    feat: 검색으로 Book list를 불러오기 위한 getBookList() 오버로딩하여 추가

--- injucksung/src/main/java/com/injucksung/injucksung/service/BookService.java
+++ injucksung/src/main/java/com/injucksung/injucksung/service/BookService.java
@@ -5,8 +5,14 @@ import org.springframework.data.domain.Page;
 
 public interface BookService {
     void addBook(Book book);
+
     void deleteBook(Long id);
+
     void updateBook(Book book);
+
     Page<Book> getBookList(int start);
+
+    Page<Book> getBookList(int start, String searchType, String searchWord);
+
     Book getBook(Long id);
 }
