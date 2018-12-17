251d25dd5ed481d7b5161568772665936c593307
siyoon <jung_210@naver.com>
2018-12-17 15:32:06 +0900

    refactor: System.out.println -> log.info() 로 변경

--- injucksung/src/test/java/com/injucksung/injucksung/repository/Print.java
+++ injucksung/src/test/java/com/injucksung/injucksung/repository/Print.java
@@ -1,21 +1,23 @@
 package com.injucksung.injucksung.repository;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 
 import java.util.Set;
 
+@Slf4j
 public class Print {
     public static void print(Page<?> page) {
-        System.out.println("totalElements: " + page.getTotalElements());
-        System.out.println("totalPages: " + page.getTotalPages());
+        log.info("totalElements: " + page.getTotalElements());
+        log.info("totalPages: " + page.getTotalPages());
         for (Object element : page) {
-            System.out.println(element.toString());
+            log.info(element.toString());
         }
     }
     public static void print(Set<?> set) {
-        System.out.println("setSize: " + set.size());
+        log.info("setSize: " + set.size());
         for (Object element : set) {
-            System.out.println(element.toString());
+            log.info(element.toString());
         }
     }
 }
