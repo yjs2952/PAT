e147d89cd0460acf358cf594020c0ee057146060
siyoon <jung_210@naver.com>
2018-12-17 18:37:15 +0900

    test: List<> Print 메소드 추가, BookContetntRepositoryTest 추가

--- injucksung/src/test/java/com/injucksung/injucksung/repository/BookContentRepositoryTest.java
+++ injucksung/src/test/java/com/injucksung/injucksung/repository/BookContentRepositoryTest.java
@@ -4,15 +4,13 @@ import com.injucksung.injucksung.domain.BookContent;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import static com.injucksung.injucksung.repository.Print.print;
+import java.util.List;
+
+import static jdk.nashorn.internal.objects.Global.print;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -23,15 +21,8 @@ public class BookContentRepositoryTest {
 
     @Test
     public void bookId로_책목차_조회하기() throws Exception {
-        Pageable pageable = PageRequest.of(0, 5);
-
-        Page<BookContent> bookContents = bookContentRepository.findBookContentByBookId(1L, pageable);
+        List<BookContent> bookContents = bookContentRepository.findBookContentByBookId(1L);
         print(bookContents);
     }
 
-    @Test
-    public void id로_책목차_한건_조회하기() throws Exception {
-        BookContent bookContent = bookContentRepository.findBookContentById(3L);
-        System.out.println(bookContent.toString());
-    }
 }
--- injucksung/src/test/java/com/injucksung/injucksung/repository/Print.java
+++ injucksung/src/test/java/com/injucksung/injucksung/repository/Print.java
@@ -3,6 +3,7 @@ package com.injucksung.injucksung.repository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 
+import java.util.List;
 import java.util.Set;
 
 @Slf4j
@@ -17,6 +18,12 @@ public class Print {
     public static void print(Set<?> set) {
         log.info("setSize: " + set.size());
         for (Object element : set) {
+            log.info(element.toString());
+        }
+    }
+    public static void print(List<?> list) {
+        log.info("setSize: " + list.size());
+        for (Object element : list) {
             log.info(element.toString());
         }
     }
