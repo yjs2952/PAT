72f6483a5aff9856189edd8c4003e2461bac13ca
siyoon <jung_210@naver.com>
2018-12-18 18:15:56 +0900

    refactor: Change annotation of QuestionCategory @AllArgs -> @RequierdArgs

--- injucksung/src/main/java/com/injucksung/injucksung/domain/QuestionCategory.java
+++ injucksung/src/main/java/com/injucksung/injucksung/domain/QuestionCategory.java
@@ -4,24 +4,24 @@ import lombok.*;
 
 import javax.persistence.*;
 
-@ToString
 @Entity
 @Table(name = "question_category")
-@Setter
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
+@Setter @Getter
+@NoArgsConstructor @RequiredArgsConstructor
 public class QuestionCategory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NonNull
     @Column(length = 100, nullable = false)
     private String name;
 
+    @NonNull
     @Column
     private Long parentId;
 
+    @NonNull
     @Column
     private String type; //인성 "per" 적성 "apt
 }
