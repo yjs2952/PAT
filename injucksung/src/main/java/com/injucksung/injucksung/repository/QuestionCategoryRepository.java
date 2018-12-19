88de06fa84d1fa4a1321be03918978d151533d3d
siyoon <jung_210@naver.com>
2018-12-19 17:53:46 +0900

    feat: [추가] 책목차id로 책목차 조회하기

--- injucksung/src/main/java/com/injucksung/injucksung/repository/QuestionCategoryRepository.java
+++ injucksung/src/main/java/com/injucksung/injucksung/repository/QuestionCategoryRepository.java
@@ -1,13 +1,10 @@
 package com.injucksung.injucksung.repository;
 
-import com.injucksung.injucksung.domain.Book;
 import com.injucksung.injucksung.domain.QuestionCategory;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface QuestionCategoryRepository extends JpaRepository<QuestionCategory, Long> {
-
+    QuestionCategory findQuestionCategoryById(Long id);
 }
 
