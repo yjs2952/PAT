cbb5d1789e6f254094c1cf4dc5fb26f493677f24
siyoon <jung_210@naver.com>
2018-12-20 17:05:33 +0900

    feat: [추가] @Transactinal 애노테이션 QuestionService

--- injucksung/src/main/java/com/injucksung/injucksung/service/QuestionServiceImpl.java
+++ injucksung/src/main/java/com/injucksung/injucksung/service/QuestionServiceImpl.java
@@ -4,31 +4,36 @@ import com.injucksung.injucksung.domain.Question;
 import com.injucksung.injucksung.repository.QuestionRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class QuestionServiceImpl implements QuestionService {
-    private QuestionRepository questionService;
+    private QuestionRepository questionRepository;
 
-    public QuestionServiceImpl(QuestionRepository questionService) {
-        this.questionService = questionService;
+    public QuestionServiceImpl(QuestionRepository questionRepository) {
+        this.questionRepository = questionRepository;
     }
 
     @Override
+    @Transactional
     public int addQuestion(Question question) {
         return 0;
     }
 
     @Override
+    @Transactional
     public void deleteQuestion(Long questionId) {
 
     }
 
     @Override
+    @Transactional
     public int modifyQuestion(Question question) {
         return 0;
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Page<Question> getQuestionList(Long bookContentId) {
         return null;
     }
