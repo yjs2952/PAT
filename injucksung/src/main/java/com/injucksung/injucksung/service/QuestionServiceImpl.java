4e442d02a2ba85ecb45d446e437a0bada23adb08
siyoon <jung_210@naver.com>
2018-12-20 17:10:05 +0900

    feat: [추가] QCRepoistory, BCResitory 필드로 추가

--- injucksung/src/main/java/com/injucksung/injucksung/service/QuestionServiceImpl.java
+++ injucksung/src/main/java/com/injucksung/injucksung/service/QuestionServiceImpl.java
@@ -1,6 +1,9 @@
 package com.injucksung.injucksung.service;
 
 import com.injucksung.injucksung.domain.Question;
+import com.injucksung.injucksung.domain.QuestionCategory;
+import com.injucksung.injucksung.repository.BookContentRepository;
+import com.injucksung.injucksung.repository.QuestionCategoryRepository;
 import com.injucksung.injucksung.repository.QuestionRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
@@ -9,14 +12,19 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class QuestionServiceImpl implements QuestionService {
     private QuestionRepository questionRepository;
+    private QuestionCategoryRepository questionCategoryRepository;
+    private BookContentRepository bookContentRepository;
 
-    public QuestionServiceImpl(QuestionRepository questionRepository) {
+    public QuestionServiceImpl(QuestionRepository questionRepository, QuestionCategoryRepository questionCategoryRepository, BookContentRepository bookContentRepository) {
         this.questionRepository = questionRepository;
+        this.questionCategoryRepository = questionCategoryRepository;
+        this.bookContentRepository = bookContentRepository;
     }
 
     @Override
     @Transactional
     public int addQuestion(Question question) {
+        
         return 0;
     }
 
