3c1c1657661c9b51818f9a1453e4d62985f8ae2f
siyoon <jung_210@naver.com>
2018-12-26 11:07:08 +0900

    feat: [구현] 관리자페이지에서  책 리스트 불러오기

--- injucksung/src/main/java/com/injucksung/injucksung/controller/AdminController.java
+++ injucksung/src/main/java/com/injucksung/injucksung/controller/AdminController.java
@@ -1,17 +1,33 @@
 package com.injucksung.injucksung.controller;
 
+import com.injucksung.injucksung.domain.Book;
+import com.injucksung.injucksung.service.BookService;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AdminController {
+    private BookService bookService;
+
+    public AdminController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
     @GetMapping("/admin")
     public String adminMain() {
         return "admin/index";
     }
 
+    //책 리스트 가져오기
     @GetMapping("/admin/book")
-    public String book() {
+    public String bookList(@RequestParam(value = "start", defaultValue = "0") int start,
+                           @RequestParam(value = "searchWord", defaultValue = "") String searchWord,
+                           Model model) {
+        Page<Book> bookList = bookService.getBookList(start);
+        model.addAttribute("bookList", bookList.getContent());
         return "admin/book";
     }
 
