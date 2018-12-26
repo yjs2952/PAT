5816de6ed68c38670dc42ba6c431fc3be1c64d7f
siyoon <jung_210@naver.com>
2018-12-26 12:07:30 +0900

    feat: [구현] 책 하나 등록하기

--- injucksung/src/main/java/com/injucksung/injucksung/controller/AdminController.java
+++ injucksung/src/main/java/com/injucksung/injucksung/controller/AdminController.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -22,17 +23,28 @@ public class AdminController {
     }
 
     //책 리스트 가져오기
-    @GetMapping("/admin/book")
+    @GetMapping("/admin/book/list")
     public String bookList(@RequestParam(value = "start", defaultValue = "0") int start,
                            @RequestParam(value = "searchWord", defaultValue = "") String searchWord,
                            Model model) {
         Page<Book> bookList = bookService.getBookList(start);
         model.addAttribute("bookList", bookList.getContent());
-        return "admin/book";
+        return "admin/book/list";
     }
 
-    @GetMapping("/admin/question")
+    //책 등록하기
+    @GetMapping("/admin/book/add")
+    public String addBook() {
+        return "/admin/book/add";
+    }
+    @PostMapping("/admin/book/add")
+    public String addBook(Book book) {
+        bookService.addBook(book);
+        return "redirect:/admin/book/list";
+    }
+
+    @GetMapping("/admin/question/list")
     public String question() {
-        return "admin/question";
+        return "admin/question/list";
     }
 }
