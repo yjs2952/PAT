06ed4d6198a6934f8a5fc80fad5337a1e2cea6bd
siyoon <jung_210@naver.com>
2018-12-27 14:44:25 +0900

    feat: [구현중] 책목차 하위로 추가하기 구현 (sequence정렬하기 미구현

--- injucksung/src/test/java/com/injucksung/injucksung/repository/BookContentRepositoryTest.java
+++ injucksung/src/test/java/com/injucksung/injucksung/repository/BookContentRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.injucksung.injucksung.repository;
 
 import com.injucksung.injucksung.domain.BookContent;
+import lombok.NonNull;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -28,6 +29,22 @@ public class BookContentRepositoryTest {
     public void 책_목차ID로_책목차_한건_조회하기() throws Exception {
         BookContent bookContentById = bookContentRepository.findBookContentById(1L);
         Assert.assertNotNull(bookContentById);
+    }
+
+    @Test
+    public void 책목차id와_sequence를_받아서_sequence_재정렬하기() throws Exception {
+        Long bookId = 1L;
+        Long groupId = 1L;
+        int sequnece = 2;
+
+        int sequence = bookContentRepository.findBookContentById(4L).getSequence();
+        System.out.println(sequence);
+        bookContentRepository.arrangeSequence();
+        bookContentRepository.flush();
+        int sequence2 = bookContentRepository.findBookContentById(4L).getSequence();
+        System.out.println(sequence2);
+
+        Assert.assertEquals(1, sequence2 - sequence);
     }
 
 }
