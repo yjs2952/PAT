f015e3db50699ac8e84af440b5e21e96f6f1fa88
siyoon <jung_210@naver.com>
2018-12-28 11:49:54 +0900

    feat: [변경] 서브 책목차 추가하기 DTO와 REST식으로 변경

--- injucksung/src/main/java/com/injucksung/injucksung/dto/BookDetail.java
+++ injucksung/src/main/java/com/injucksung/injucksung/dto/BookDetail.java
@@ -6,7 +6,7 @@ import lombok.Setter;
 @Getter @Setter
 public class BookDetail {
     private boolean editTopBookContent;
-    private int editSubBookContentId;
+    private int addSubBookContentParentId;
 
     //롬복 Getter로 미생성 되어 따로 명시
     public boolean isEditTopBookContent() {
