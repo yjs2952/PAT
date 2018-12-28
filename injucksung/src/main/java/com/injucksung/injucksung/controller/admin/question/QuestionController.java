07989a67d04935a58253f0d9bf405d916c6cf105
siyoon <jung_210@naver.com>
2018-12-28 18:10:29 +0900

    feat: [구현] 문제 카테고리 추가하기

--- injucksung/src/main/java/com/injucksung/injucksung/controller/admin/question/QuestionController.java
+++ injucksung/src/main/java/com/injucksung/injucksung/controller/admin/question/QuestionController.java
@@ -1,32 +1,14 @@
 package com.injucksung.injucksung.controller.admin.question;
 
-import com.injucksung.injucksung.domain.Book;
-import com.injucksung.injucksung.domain.BookContent;
-import com.injucksung.injucksung.dto.BookDetail;
-import com.injucksung.injucksung.service.BookContentService;
-import com.injucksung.injucksung.service.BookService;
-import com.injucksung.injucksung.service.QuestionService;
+import com.injucksung.injucksung.service.QuestionCategoryService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/admin/questions")
-public class QuestionController {
-    private final QuestionService questionService;
-
-    //책 리스트 가져오기
-    @GetMapping
-    public String QuestionList(@RequestParam(value = "start", defaultValue = "0") int start,
-                           Model model) {
-//        Page<Book> bookList = bookService.getBookList(start);
-//        questionService.getQuestionList()
-//        model.addAttribute("bookList", bookList.getContent());
-        return "admin/books/list";
-    }
-}
+public class QuestionController {}
