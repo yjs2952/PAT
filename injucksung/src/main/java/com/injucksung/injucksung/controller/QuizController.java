4a33e27ce613dbb96317f49e2d910451d4daf943
siyoon <jung_210@naver.com>
2019-01-03 18:30:20 +0900

    feat: [추가] /quiz 매핑된 콘트롤러 메소드 구현

--- injucksung/src/main/java/com/injucksung/injucksung/controller/QuizController.java
+++ injucksung/src/main/java/com/injucksung/injucksung/controller/QuizController.java
@@ -1,10 +1,18 @@
 package com.injucksung.injucksung.controller;
 
+import com.injucksung.injucksung.domain.Question;
+import com.injucksung.injucksung.dto.BookContentSelectForm;
 import com.injucksung.injucksung.service.QuestionService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -13,8 +21,20 @@ public class QuizController {
     private final QuestionService questionService;
 
     //문제 리스트 가져오기
-    @GetMapping String getQuestionList() {
+    @GetMapping
+    public ModelAndView getQuestionList(@ModelAttribute BookContentSelectForm bookContentSelectForm,
+                                        ModelAndView modelAndView) {
+        // TODO: checkbox를 하나도 선택 안한 경우의 예외처리
+        // TODO: for문 말고 JPA에서 해결하도록 하자
+        List<Question> questions = new ArrayList<>();
+        for (Long bookContentId : bookContentSelectForm.getBookContentId()) {
+             questions.addAll(questionService.getQuestionList(bookContentId).getContent());
+        }
+        modelAndView.addObject("questions", questions);
 
+        if (bookContentSelectForm.getAction().equals("문제풀기")) modelAndView.setViewName("/quiz/grade");
+        else modelAndView.setViewName("/quiz/solve");
+        return modelAndView;
     }
 
 }
