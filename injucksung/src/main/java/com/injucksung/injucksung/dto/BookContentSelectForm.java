cc240742d8ee697d437ff6452d42eadf7c10b87b
siyoon <jung_210@naver.com>
2019-01-03 18:29:41 +0900

    feat: [구현] BookContentForm DTO 추가

--- injucksung/src/main/java/com/injucksung/injucksung/dto/BookContentSelectForm.java
+++ injucksung/src/main/java/com/injucksung/injucksung/dto/BookContentSelectForm.java
@@ -5,27 +5,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class BookContentForm {
-    private String name;
-    private Boolean isMockTest;
-    private Integer recommendTime;
-    private Long bookId;
-    private Long groupId;
-    private Integer sequence;
-    private Integer depth;
-    private Integer questionCount;
-
-    public BookContentForm(String name, Boolean isMockTest, Integer recommendTime, Long bookId, Long groupId, Integer sequence, Integer depth) {
-        this.name = name;
-        this.isMockTest = isMockTest;
-        this.recommendTime = recommendTime;
-        this.bookId = bookId;
-        this.groupId = groupId;
-        this.sequence = sequence;
-        this.depth = depth;
-        this.questionCount = 0;
-
-        if (sequence == null) this.sequence = 0;
-        if (depth == null) this.depth = 0;
-    }
+public class BookContentSelectForm {
+    private Long[] bookContentId;
+    private String action;
 }
