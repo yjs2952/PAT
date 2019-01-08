7471d9f10c9f87333beabe16a2c30eafc32edd4e
siyoon <jung_210@naver.com>
2019-01-08 12:10:39 +0900

    feat: WHERE IN 을 이용하여서 조건이 여러개인 경우 List로 넘기기 구현
    와 진짜 이걸로 2일 삽질 했는데 개간단데스요 후..

--- injucksung/src/main/java/com/injucksung/injucksung/repository/QuestionRepository.java
+++ injucksung/src/main/java/com/injucksung/injucksung/repository/QuestionRepository.java
@@ -1,6 +1,7 @@
 package com.injucksung.injucksung.repository;
 
 import com.injucksung.injucksung.domain.Question;
+import com.injucksung.injucksung.dto.BookContentForm;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,6 +14,10 @@ public interface QuestionRepository extends JpaRepository<Question, Long> {
     //책 목차 id로 문제 조회하기
     @Query(value = "SELECT q FROM Question q JOIN FETCH q.bookContent bc JOIN FETCH q.questionCategory qc WHERE book_content_id=:bookContentId")
     List<Question> findQuestionByBookContentId(@Param("bookContentId") Long bookContentId);
+
+    //책 목차 id가 여러개인 경우(list) 해당 문제 조회하기
+    @Query(value = "SELECT q FROM Question q JOIN FETCH q.bookContent bc JOIN FETCH q.questionCategory qc WHERE book_content_id IN :bookContentIds")
+    List<Question> findQuestionByBookContentId(@Param("bookContentIds") List<Long> bookContentIds);
 
     //책 id로 문제 조회하기
     //@Query(value = "SELECT * FROM question q JOIN book_content bc ON q.book_content_id = bc.id WHERE bc.book_id =:bookId", nativeQuery = true)
