a6412e981c5fd09997786ef70b104d2401077112
siyoon <jung_210@naver.com>
2019-01-08 12:29:10 +0900

    style: 배열 변수명을 복수로 변경

--- injucksung/src/main/java/com/injucksung/injucksung/dto/SelectedBookContentForQuizForm.java
+++ injucksung/src/main/java/com/injucksung/injucksung/dto/SelectedBookContentForQuizForm.java
@@ -6,6 +6,6 @@ import lombok.Setter;
 @Getter
 @Setter
 public class SelectedBookContentForQuizForm {
-    private Long[] bookContentId;
+    private Long[] bookContentIds;
     private String action;
 }
