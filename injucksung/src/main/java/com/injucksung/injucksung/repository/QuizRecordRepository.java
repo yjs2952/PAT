5d9541065193a26c7c4c5d5d5cf10d37bc7c766b
siyoon <jung_210@naver.com>
2019-01-09 13:23:21 +0900

    feat: 시험 목록 유저ID로 불러오기 구현

--- injucksung/src/main/java/com/injucksung/injucksung/repository/QuizRecordRepository.java
+++ injucksung/src/main/java/com/injucksung/injucksung/repository/QuizRecordRepository.java
@@ -1,7 +1,14 @@
 package com.injucksung.injucksung.repository;
 
 import com.injucksung.injucksung.domain.QuizRecord;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface QuizRecordRepository extends JpaRepository<QuizRecord, Long> {
+    //유저 한명의 시험 목록(QuizRecord) 가져오기
+    @Query(value = "SELECT qr FROM QuizRecord qr WHERE user_id = :userId")
+    Page<QuizRecord> findQuizRecordByUserId(@Param("userId") Long userId, Pageable pageable);
 }
