b548ba47dfa1844c83166b0f8e1f1f66d0ae35d8
siyoon <jung_210@naver.com>
2019-01-09 13:01:40 +0900

    feat: 로그인 여부 확인하여 인덱스 페이지 네비게이션 버튼 설정

--- injucksung/src/main/java/com/injucksung/injucksung/controller/MainController.java
+++ injucksung/src/main/java/com/injucksung/injucksung/controller/MainController.java
@@ -1,15 +1,28 @@
 package com.injucksung.injucksung.controller;
 
+import com.injucksung.injucksung.security.CustomUserDetails;
 import lombok.extern.java.Log;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @Log
 public class MainController {
     @GetMapping("/")
-    public String main() {
+    public String main(Model model) {
         log.info("<<<<<Start MainController>>>>>");
+
+        //로그인 정보를 가져와서 캐스팅 가능한 경우만 addAttribute를 진행한다.
+        //TODO 최선일까?
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof CustomUserDetails) {
+            model.addAttribute("user", principal);
+        }
+
         return "/index";
     }
 }
