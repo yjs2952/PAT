e67ec07411486c52606a2e3f8b98261905f67e27
siyoon <jung_210@naver.com>
2019-01-09 13:31:24 +0900

    feat: 시험 목록 불러오기 서비스 구현

--- injucksung/src/main/java/com/injucksung/injucksung/domain/enums/PageSize.java
+++ injucksung/src/main/java/com/injucksung/injucksung/domain/enums/PageSize.java
@@ -5,6 +5,6 @@ import lombok.Getter;
 
 @Getter @AllArgsConstructor
 public enum PageSize {
-    BOOK(10), QUESTION_CATEGORY(20), QUESTION(10), USER(10);
+    BOOK(10), QUESTION_CATEGORY(20), QUESTION(10), USER(10), QUIZ_RECORD(10);
     private final int limit;
 }
