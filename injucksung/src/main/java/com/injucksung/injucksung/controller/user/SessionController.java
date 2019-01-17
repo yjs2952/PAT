2f481c4cc0601d42f13c6c41266cf57bf83ad8f8
siyoon <jung_210@naver.com>
2019-01-17 20:04:52 +0900

    fix: login POST 매핑 없어서 추가함

--- injucksung/src/main/java/com/injucksung/injucksung/controller/user/SessionController.java
+++ injucksung/src/main/java/com/injucksung/injucksung/controller/user/SessionController.java
@@ -3,6 +3,7 @@ package com.injucksung.injucksung.controller.user;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,6 +13,11 @@ public class SessionController {
     @GetMapping
     public String getLoginForm() {
         return "/session/login";
+    }
+
+    @PostMapping
+    public String login() {
+        return "/";
     }
 
     //로그아웃
