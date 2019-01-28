2918ac317db5e45e61ebc45a9568bfef39d20a70
siyoon <jung_210@naver.com>
2019-01-28 20:34:48 +0900

    feat: 스프링 시큐리티로 인증여부 확인하고 nav메뉴 구성하기

--- injucksung/src/main/java/com/injucksung/injucksung/config/WebMvcConfig.java
+++ injucksung/src/main/java/com/injucksung/injucksung/config/WebMvcConfig.java
@@ -1,9 +1,7 @@
 package com.injucksung.injucksung.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
@@ -11,12 +9,6 @@ import org.springframework.web.servlet.resource.PathResourceResolver;
 @Configuration
 @RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-        return bCryptPasswordEncoder;
-    }
-
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
@@ -26,6 +18,4 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .resourceChain(true)
                 .addResolver(new PathResourceResolver());
     }
-
-
 }
